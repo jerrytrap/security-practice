dc57b8a865c36e8b71c9e1a6e0e08c33662f601e
류대현 <eogus1027@naver.com>
2025-03-18 02:04:14 +0900

    댓글 수정 테스트

--- src/main/java/com/example/security/domain/report/comment/controller/ApiV1ReportCommentController.java
+++ src/main/java/com/example/security/domain/report/comment/controller/ApiV1ReportCommentController.java
@@ -9,7 +9,10 @@ import com.example.security.domain.student.Student;
 import com.example.security.global.Rq;
 import com.example.security.global.RsData;
 import com.example.security.global.ServiceException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -57,6 +60,40 @@ public class ApiV1ReportCommentController {
         return new RsData<>(
                 "200-1",
                 "%d번 댓글이 삭제되었습니다.".formatted(id)
+        );
+    }
+
+    record CommentModifyReqBody(
+            @NotBlank
+            @Length(min = 2, max = 100)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    public RsData<CommentDto> modify(
+            @PathVariable long reportId,
+            @PathVariable long id,
+            @RequestBody @Valid CommentModifyReqBody reqBody
+    ) {
+        Student actor = rq.checkAuthentication();
+
+        Report report = reportService.findById(reportId).orElseThrow(
+                () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(reportId))
+        );
+
+        Comment comment = report.getCommentById(id).orElseThrow(
+                () -> new ServiceException("404-2", "%d번 댓글은 존재하지 않습니다.".formatted(id))
+        );
+
+        comment.checkActorCanModify(actor);
+
+        comment.modify(reqBody.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 댓글이 수정되었습니다.".formatted(id),
+                new CommentDto(comment)
         );
     }
 }
